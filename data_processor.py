f8ad6bfb96ddc7ed606f26f2d561c21cd21a7f16
Lauri Sarap <lauri.aardla@gmail.com>
2024-02-05 21:30:56 +0200

    cycles

--- data_processor.py
+++ data_processor.py
@@ -35,3 +35,71 @@ def preprocess_data(data_dir, processed_dir):
             save_path = os.path.join(processed_dir, new_file_name)
             processed_data.to_csv(save_path, index=False, sep=',')
     print("Data preprocessing complete!")
+
+
+def identify_cycles(data_file):
+    charging_start_time = None
+    discharging_start_time = None
+    cycles = []
+
+    # Iterate over the rows of the DataFrame
+    for index, row in data_file.iterrows():
+        output_voltage = row['Output Voltage']
+        current_time = row['Time']
+
+        if charging_start_time is None and output_voltage > 1:
+            charging_start_time = current_time
+        if charging_start_time is None and output_voltage < 1:
+            discharging_start_time = current_time
+
+        elif charging_start_time is not None and discharging_start_time is None and output_voltage < 3:
+            discharging_start_time = current_time
+
+        elif discharging_start_time is not None and output_voltage > 3:
+            cycles.append({
+                'Charging Start': charging_start_time,
+                'Charging End': np.round(discharging_start_time - 0.001, 3),
+                'Discharging Start': discharging_start_time,
+                'Discharging End': np.round(current_time - 0.001, 3),
+                'Period Start': charging_start_time,
+                'Period End': np.round(current_time - 0.001, 3)
+            })
+            # Reset for the next cycle
+            charging_start_time = current_time
+            discharging_start_time = None
+
+    if cycles:
+        cycles.pop(0)
+
+    for i, cycle in enumerate(cycles, 1):
+        print(f"Cycle {i}:")
+        print(f"  Charging Start: {cycle['Charging Start']}, Charging End: {cycle['Charging End']}")
+        print(f"  Discharging Start: {cycle['Discharging Start']}, Discharging End: {cycle['Discharging End']}")
+        print(f"  Total Period Start: {cycle['Period Start']}, Total Period End: {cycle['Period End']}")
+
+    # Print the total number of full cycles
+    print(f"Total number of full cycles found: {len(cycles)}")
+
+    return cycles
+
+
+def calculate_periods(cycles, cycle_dir, new_cycles_file_name):
+    cycle_times_data = []
+
+    for cycle in cycles:
+        charge_time = cycle['Charging End'] - cycle['Charging Start']
+        discharge_time = cycle['Discharging End'] - cycle['Discharging Start']
+        total_period = cycle['Period End'] - cycle['Period Start']
+
+        cycle_times_data.append({
+            'Charge Time': round(charge_time, 3),
+            'Discharge Time': round(discharge_time, 3),
+            'Total Period': round(total_period, 3)
+        })
+
+    # Convert the new list to a DataFrame
+    cycle_times_df = pandas.DataFrame(cycle_times_data)
+
+    # Save the new DataFrame to a CSV file
+    cycle_times_file_path = os.path.join(cycle_dir, new_cycles_file_name)
+    cycle_times_df.to_csv(cycle_times_file_path, index=False, sep=',')
--- main.py
+++ main.py
@@ -5,53 +5,23 @@ import data_processor
 
 data_dir = "data/"
 processed_dir = "processed/"
+cycle_dir = "cycles/"
 if not os.path.exists(processed_dir):
     os.makedirs(processed_dir)
+if not os.path.exists(cycle_dir):
+    os.makedirs(cycle_dir)
+
+# Preprocess data
 #data_processor.preprocess_data(data_dir, processed_dir)
 
-# Data analysis
-test_file_path = os.path.join(processed_dir, "processed_Rb_5M_1000Hz_2024-02-05_13-48-16.csv")
+# Identify cycles
+current_test_file = "processed_Ra_1M_1000Hz_2024-02-05_13-16-00.csv"
+
+test_file_path = os.path.join(processed_dir, current_test_file)
 test_data = pandas.read_csv(test_file_path, sep=',')
+cycles = data_processor.identify_cycles(test_data)
 
-# Initialize variables to track the charging and discharging phases
-charging_start_time = None
-discharging_start_time = None
-cycles = []
-
-# Iterate over the rows of the DataFrame
-for index, row in test_data.iterrows():
-    output_voltage = row['Output Voltage']
-    current_time = row['Time']
-
-    if charging_start_time is None and output_voltage > 1:
-        charging_start_time = current_time
-    if charging_start_time is None and output_voltage < 1:
-        discharging_start_time = current_time
-
-    elif charging_start_time is not None and discharging_start_time is None and output_voltage < 3:
-        discharging_start_time = current_time
-
-    elif discharging_start_time is not None and output_voltage > 3:
-        cycles.append({
-            'Charging Start': charging_start_time,
-            'Charging End': np.round(discharging_start_time - 0.001, 3),
-            'Discharging Start': discharging_start_time,
-            'Discharging End': np.round(current_time - 0.001, 3),
-            'Period Start': charging_start_time,
-            'Period End': np.round(current_time - 0.001, 3)
-        })
-        # Reset for the next cycle
-        charging_start_time = current_time
-        discharging_start_time = None
-
-if cycles:
-    cycles.pop(0)
-
-for i, cycle in enumerate(cycles, 1):
-    print(f"Cycle {i}:")
-    print(f"  Charging Start: {cycle['Charging Start']}, Charging End: {cycle['Charging End']}")
-    print(f"  Discharging Start: {cycle['Discharging Start']}, Discharging End: {cycle['Discharging End']}")
-    print(f"  Total Period Start: {cycle['Period Start']}, Total Period End: {cycle['Period End']}")
-
-# Print the total number of full cycles
-print(f"Total number of full cycles found: {len(cycles)}")
+# Calculate periods
+original_file_name = current_test_file.replace('processed_', '')
+new_cycles_file_name = f"cycles_{original_file_name}"
+periods = data_processor.calculate_periods(cycles, cycle_dir=cycle_dir, new_cycles_file_name=new_cycles_file_name)
